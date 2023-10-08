ab03d9bd5c90ec60a068153d5e977422ca4cbad5
Alisher Eshmurodov <eshmurodov.cooder@gmail.com>
2023-10-08 18:24:29 +0500

    little changes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,21 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+
+    public function response($message = null, $data = null)
+    {
+
+    }
+
+
+    public function success($message = null, $data = null)
+    {
+
+    }
+
+    public function error($message = null, $data = null)
+    {
+
+    }
 }
