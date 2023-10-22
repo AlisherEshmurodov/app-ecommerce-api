03a637fab6439460cc005c69a6704da1b1b49dba
Alisher Eshmurodov <eshmurodov.cooder@gmail.com>
2023-10-22 13:26:46 +0500

    added filter for date to Order count and sales-sum funcs for admin

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -8,21 +8,36 @@ use Illuminate\Support\Carbon;
 
 class StatsController extends Controller
 {
-    public function ordersCount()
+    public function ordersCount(Request $request)
     {
+        $from = Carbon::now()->subMonth();
+        $to = Carbon::now();
+        if ($request->has(['from', 'to'])){
+            $from = $request->from;
+            $to = $request->to;
+        }
+
+
         return $this->response(
             Order::query()
-                ->where("created_at", ">=", Carbon::now()->subMonth())
+                ->whereBetween("created_at", [$from, Carbon::parse($to)->endOfDay()])
                 ->whereRelation("status", "code", "closed")
                 ->count()
         );
     }
 
-    public function ordersSalesSum()
+    public function ordersSalesSum(Request $request)
     {
+        $from = Carbon::now()->subMonth();
+        $to = Carbon::now();
+        if ($request->has(['from', 'to'])){
+            $from = $request->from;
+            $to = $request->to;
+        }
+
         return $this->response(
             Order::query()
-                ->where("created_at", ">=", Carbon::now()->subMonth())
+                ->whereBetween("created_at", [$from, Carbon::parse($to)->endOfDay()])
                 ->whereRelation("status", "code", "closed")
                 ->sum("total_sum")
         );
